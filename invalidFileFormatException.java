2e5dd61d03941013c400bc2f6b6aac23a1e4b9b5
Marios Katsigiannis <mk59@hw.ac.uk>
2016-02-02 15:11:47 +0000

    Added a serialization UID

--- invalidFileFormatException.java
+++ invalidFileFormatException.java
@@ -1,6 +1,11 @@
 
 public class invalidFileFormatException extends Exception {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public invalidFileFormatException(String filename, int line) {
 		super("Invalid file format at line " + line + " in file " + filename);
 	}
