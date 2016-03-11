3deb276c68305f98c7814dbaa85d7fbb629b941a
Marios Katsigiannis <mk59@hw.ac.uk>
2016-03-11 21:27:15 +0000

    Added the waiter class will add interface and maybe implementation later today...

---
+++ F21AS/Waiter.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package F21AS;
+
+/**
+ * @author Marios Katsigiannis
+ * This class will be used to deliver the dishes to the tables from the kitchen
+ */
+public class Waiter {
+	// I really have no idea right now what to do (:P}
+}
